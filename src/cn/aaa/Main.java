68a11d235e25b004ecf0de3a924df61653f4b566
zjavax <1306187161@qq.com>
2021-08-15 23:24:13 +0800

    java8 源码，初次提交

---
+++ src/cn/aaa/Main.java
@@ -0,0 +1,14 @@
+package cn.aaa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap m = new HashMap<String, String>();
+        m.put("1", "1");
+        System.out.println("123");
+    }
+}
